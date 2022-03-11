b018140de86d0f684ba55bc083070658a054c85f
FaizievRT <rus23f@mail.ru>
2022-03-12 00:25:35 +0300

    Реализовать пользовательские фильтры для площадок машин v2

--- src/main/java/salescars/AdRepository.java
+++ src/main/java/salescars/AdRepository.java
@@ -6,41 +6,74 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.List;
+import java.util.function.Function;
+
 public class AdRepository {
-    public static void main(String[] args) {
-        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure().build();
-        SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    private StandardServiceRegistry registry;
+    private SessionFactory sf;
+
+    public AdRepository() {
+        this.registry = new StandardServiceRegistryBuilder().configure().build();
+        this.sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    private static final class Lazy {
+        private static final AdRepository INST = new AdRepository();
+    }
+
+    public static AdRepository instOf() {
+        return AdRepository.Lazy.INST;
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
         Session session = sf.openSession();
+        session.beginTransaction();
         try {
-            session.beginTransaction();
-
-            var lst = session.createQuery(
-                            "select distinct an from Announcement an "
-                                    + "join fetch an.car cr "
-                                    + "where an.created >= current_date at time zone 'UTC' - interval '1 days'").list();
-            lst.forEach(System.out::println);
-
-            var lst2 = session.createQuery(
-                            "select distinct an from Announcement an "
-                                    + "join fetch an.car cr "
-                                    + "where cr.photo != ''").list();
-            lst2.forEach(System.out::println);
-
-            var lst3 = session.createQuery(
-                    "select distinct an from Announcement an "
-                            + "join fetch an.car cr "
-                            + "where cr.model = :model")
-                    .setParameter("model", "BMW")
-                    .list();
-            lst3.forEach(System.out::println);
-
-             session.getTransaction().commit();
-        } catch (Exception e) {
+            T rsl = command.apply(session);
+            session.getTransaction().commit();
+            return rsl;
+        } catch (final Exception e) {
             session.getTransaction().rollback();
-            e.printStackTrace();
+            throw e;
         } finally {
             session.close();
         }
+    }
+
+    public List<Announcement> selectDay() {
+        return tx(session -> {
+                    return session.createQuery("select distinct an from Announcement an "
+                    + "join fetch an.car cr "
+                    + "where an.created >= current_date at time zone 'UTC' - interval '1 days'", Announcement.class).list();
+                }
+        );
+    }
+
+    public List<Announcement> selectFoto() {
+        return tx(session -> {
+                    return session.createQuery("select distinct an from Announcement an "
+                    + "join fetch an.car cr "
+                    + "where cr.photo != ''", Announcement.class).list();
+                }
+        );
+    }
+
+    public List<Announcement> selectModel() {
+        return tx(session -> {
+                    return session.createQuery("select distinct an from Announcement an "
+                            + "join fetch an.car cr "
+                            + "where cr.model = :model", Announcement.class)
+                            .setParameter("model", "BMW")
+                            .list();
+                }
+        );
+    }
+
+    public static void main(String[] args) {
+        AdRepository adRepository = AdRepository.instOf();
+        adRepository.selectDay().forEach(System.out::println);
+        adRepository.selectFoto().forEach(System.out::println);
+        adRepository.selectModel().forEach(System.out::println);
     }
 }
